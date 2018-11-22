f1dd403a6e6d6e5a1ac9e801e607fec2a9a6a922
DexterCABUNGCAL <DexterCABUNGCAL@DESKTOP-39F1OA4>
2018-11-22 23:28:47 +0800

    test

--- src/main/java/myspringboot/crudcloudant/user/UserController.java
+++ src/main/java/myspringboot/crudcloudant/user/UserController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-	
-	private String testProperty;
-	
+		
 	@Autowired
 	private UserService userService;
 	
